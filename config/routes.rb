ec65aee8ba83c1495bf3e621ae26e1a744c1061e
Steven Hua <stevenhua1015@gmail.com>
2016-10-21 01:31:10 -0700

    added valid signup test

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/signup',  to: 'users#new'
-  resources :users
   post '/signup',  to: 'users#create'
-
-
+  resources :users
 end
