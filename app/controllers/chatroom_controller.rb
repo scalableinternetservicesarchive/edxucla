26dec67d891dc1fbba78ced419cbc911a53719f3
Isaac Yeo <isaacyeo93@gmail.com>
2016-11-07 17:32:32 -0800

    add controllers and models for chatroom and messages

---
+++ app/controllers/chatroom_controller.rb
@@ -0,0 +1,6 @@
+class ChatroomController < ApplicationController
+	def show
+		@chatroom = Chatroom.find_by(slug: params[:slug])
+		@message = Message.new
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161025040258) do
+ActiveRecord::Schema.define(version: 20161104174751) do
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "course_users", force: :cascade do |t|
     t.integer  "user_id"
@@ -38,6 +43,11 @@ ActiveRecord::Schema.define(version: 20161025040258) do
   create_table "educations", force: :cascade do |t|
     t.string "name"
     t.string "alias"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
