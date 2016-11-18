b38321de02ab52c4de83e83724f867415598a1b3
Isaac Yeo <isaacyeo93@gmail.com>
2016-11-18 02:17:55 -0800

    add tests to education_test.rb

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,2 +1,4 @@
 class Education < ApplicationRecord
+	validates :name, presence: true
+	validates :alias, presence: true
 end
--- test/models/education_test.rb
+++ test/models/education_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class EducationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @education = Education.new(name: "Example education", alias: "Example alias")
+  end
+
+  test "should be valid" do
+    assert @education.valid?
+  end
+
+  test "education name should be present" do
+    @education.name = ""
+    assert_not @education.valid?
+  end
+
+  test "education alias should be present" do
+    @education.alias = ""
+    assert_not @education.valid?
+  end
+
 end
