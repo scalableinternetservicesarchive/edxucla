4891741ec603dc726db6255218fd2fae3677993d
Steven Hua <stevenhua1015@gmail.com>
2016-11-28 16:50:39 -0800

    Rework dashboard to link to messages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,16 +6,20 @@ class StaticPagesController < ApplicationController
       @sessions_student = TutoringSession.where('student = :student AND status = "active"', student: "#{user}")
 
       @students = []
+      @conversations_tutor = []
       i = 0
       while i < @sessions_tutor.count
         @students[i] = User.find(@sessions_tutor[i].student)
+        @conversations_tutor[i] = Conversation.where('user_one = :sender OR user_two = :receiver', sender: @students[i], receiver: @students[i])[0]
         i += 1
       end
 
       i = 0
       @tutors = []
+      @conversations_student = []
       while i < @sessions_student.count
         @tutors[i] = User.find(@sessions_student[i].tutor)
+        @conversations_student[i] = Conversation.where('user_one = :sender OR user_two = :receiver', sender: @tutors[i], receiver: @tutors[i])[0]
         i += 1
       end
     end
